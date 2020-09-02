6dc424904eb5dd31fe65e6d4523a671d4a27e35e
PawankumarPK <pk9910765616@gmail.com>
2020-09-02 15:42:58 +0530

    Add mongoDB and insert data using signup page with unique

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,88 @@
 var express = require('express');
 var router = express.Router();
 
+var userModule = require("../modules/user")
+
 /* GET login page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Password Management System' });
 });
 
+function checkEmail(req, res, next) {
+  var email = req.body.email
+  var checkExitsEmail = userModule.findOne({ email: email })
+  checkExitsEmail.exec((err, data) => {
+    if (err) throw err
+    if (data) {
+      return res.render('signup', { title: 'Password Management System', msg: "Email Already Exist" });
+    }
+    next()
+
+  })
+}
+
+function checkUsername(req, res, next) {
+  var username = req.body.uname
+  var checkExitsUsername = userModule.findOne({ username: username })
+  checkExitsUsername.exec((err, data) => {
+    if (err) throw err
+    if (data) {
+      return res.render('signup', { title: 'Password Management System', msg: "Username Already Exist" });
+    }
+    next()
+
+  })
+}
+
 /* GET signup page. */
-router.get('/signup', function(req, res, next) {
-  res.render('signup', { title: 'Password Management System' });
+router.get('/signup', function (req, res, next) {
+  res.render('signup', { title: 'Password Management System', msg: " " });
+});
+
+router.post('/signup', checkUsername, checkEmail, function (req, res, next) {
+
+  var username = req.body.uname
+  var email = req.body.email
+  var password = req.body.password
+  var confpassword = req.body.confpassword
+
+  if (password != confpassword) {
+    res.render('signup', { title: 'Password Management System', msg: "Password not matched" });
+  } else {
+    var userDetail = new userModule({
+      username: username,
+      email: email,
+      password: password
+    })
+
+    userDetail.save((err, doc) => {
+      if (err) throw err
+      res.render('signup', { title: 'Password Management System', msg: "User Register Successfully" });
+    })
+
+  }
+
 });
 
 /* Password Category. */
-router.get('/passwordCategory', function(req, res, next) {
+router.get('/passwordCategory', function (req, res, next) {
   res.render('password-category', { title: 'Password Management System' });
 });
 
 /* Add new  Category. */
-router.get('/add-new-category', function(req, res, next) {
+router.get('/add-new-category', function (req, res, next) {
   res.render('addNewCategory', { title: 'Password Management System' });
 });
 
 
 /* Add new password. */
-router.get('/add-new-password', function(req, res, next) {
+router.get('/add-new-password', function (req, res, next) {
   res.render('add-new-password', { title: 'Password Management System' });
 });
 
 
 /* view all password. */
-router.get('/view-all-password', function(req, res, next) {
+router.get('/view-all-password', function (req, res, next) {
   res.render('view-all-password', { title: 'Password Management System' });
 });
 module.exports = router;
