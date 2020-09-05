7ef63677cc4f9a185a74a00129669cc2476aeec6
PawankumarPK <pk9910765616@gmail.com>
2020-09-05 21:56:44 +0530

    Add add-new-password successfully

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ if (typeof localStorage === "undefined" || localStorage === null) {
 
 var userModule = require("../modules/user")
 var passCatModel = require("../modules/password_category")
+var passModel = require("../modules/add_password")
 var getPassCat = passCatModel.find({})
 
 function checkLoginUser(req, res, next) {
@@ -210,10 +211,28 @@ router.get('/add-new-password', checkLoginUser, function (req, res, next) {
   var loginUser = localStorage.getItem("loginUser")
   getPassCat.exec(function (err, data) {
     if (err) throw err
-    res.render('add-new-password', { title: 'Password Management System', loginUser: loginUser, records: data });
+    res.render('add-new-password', { title: 'Password Management System', loginUser: loginUser, records: data, success: "" });
   })
 
 });
+router.post('/add-new-password', checkLoginUser, function (req, res, next) {
+  var loginUser = localStorage.getItem('loginUser');
+  var pass_cat = req.body.pass_cat;
+  var pass_details = req.body.pass_details;
+  var password_details = new passModel({
+    password_category: pass_cat,
+    password_detail: pass_details
+  });
+
+  password_details.save(function (err, doc) {
+    getPassCat.exec(function (err, data) {
+      if (err) throw err;
+      res.render('add-new-password', { title: 'Password Management System', loginUser: loginUser, records: data, success: "Password Details Inserted Successfully" });
+
+    });
+
+  });
+})
 
 
 /* view all password. */
