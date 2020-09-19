abefc4f2ca65effe7a86bdd74cff8fd58d86ff83
PawankumarPK <pk9910765616@gmail.com>
2020-09-19 23:04:39 +0530

    get only particular field data n json res

--- api/add-category.js
+++ api/add-category.js
@@ -1,11 +1,12 @@
 var express = require("express")
 var router = express.Router()
 var passCatModel = require("../modules/password_category")
-var getPassCat = passCatModel.find({})
+var getPassCat = passCatModel.find({},{"password_category":1,"_id":0})
 
 router.get("/", function (req, res) {
 
     getPassCat.exec(function (err, data) {
+        if (err) throw err
         res.send(data)
     })
 })
--- modules/password_category.js
+++ modules/password_category.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.connect("mongodb://localhost:27017/pms", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-var conn = mongoose.Collection
+var conn = mongoose.connection
 
 var passcatSchema = new mongoose.Schema({
     password_category: {
