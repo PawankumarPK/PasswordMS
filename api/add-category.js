c0ca5e21322fb214bf10f92b4c3715ea96e69df9
PawankumarPK <pk9910765616@gmail.com>
2020-09-20 00:47:11 +0530

    Update Data using PATCH method successfully

--- api/add-category.js
+++ api/add-category.js
@@ -1,7 +1,7 @@
 var express = require("express")
 var router = express.Router()
 var passCatModel = require("../modules/password_category")
-var getPassCat = passCatModel.find({}, { "password_category": 1})
+var getPassCat = passCatModel.find({}, { "password_category": 1 })
 
 router.get("/getCategory", function (req, res) {
 
@@ -30,6 +30,21 @@ router.put("/add-updated-category/:id", function (req, res) {
         data.save(function (err) {
             if (err) throw err
             res.send("Update Data Successfully Using PUT Method")
+        })
+    })
+
+})
+
+router.patch("/update-category/:id", function (req, res) {
+    var id = req.params.id
+    var passCategory = req.body.pass_cat
+
+    passCatModel.findById(id, function (err, data) {
+        data.password_category = passCategory ? passCategory : data.password_category
+
+        data.save(function (err) {
+            if (err) throw err
+            res.send("Update data successfully using PATCH method")
         })
     })
 
